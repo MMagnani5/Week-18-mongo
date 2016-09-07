89b8c93db0a1da79e71e8a4ffe0295347646b6c2
Mike Magnani <mikemagnani5@gmail.com>
2016-09-07 15:14:28 -0400

    html set and dependencies

---
+++ server.js
@@ -0,0 +1,9 @@
+// dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+// Notice: Our scraping tools are prepared, too
+var request = require('request'); 
+var cheerio = require('cheerio');
