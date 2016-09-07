d0aa4565762f773046de5a9238a833d90bbd7fb7
Mike Magnani <mikemagnani5@gmail.com>
2016-09-07 16:02:04 -0400

    created function for app.js file

---
+++ models/Articles.js
@@ -0,0 +1,17 @@
+// require mongoose
+var mongoose = require('mongoose');
+// create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+  
+
+
+
+
+});
+
+
+// Create article schema
+var ArticleSchema = new Schema
