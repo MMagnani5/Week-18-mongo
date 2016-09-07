9c21d846cf7030b27c7d27cd011cb150161ed47e
Mike Magnani <mikemagnani5@gmail.com>
2016-09-07 15:45:05 -0400

    set up server file

--- server.js
+++ server.js
@@ -4,6 +4,48 @@ var app = express();
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-// Notice: Our scraping tools are prepared, too
+
+//  scraping tools 
 var request = require('request'); 
 var cheerio = require('cheerio');
+
+// use morgan and bodyparser with our app
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// make public a static dir
+app.use(express.static('public'));
+
+// Database configuration created with mongoose
+mongoose.connect('mongodb://localhost/MongoWebArticles');
+var db = mongoose.connection;
+
+// show any mongoose errors
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+
+// log into the db through mongoose
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
+
+// And we bring in our Note and Article models
+var Note = require('./models/Note.js');
+var Article = require('./models/Article.js');
+
+// Routes
+
+app.get('/', function(req, res) {
+  res.send(index.html);
+});
+
+
+
+
+
+
+
+
